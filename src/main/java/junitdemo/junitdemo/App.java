0c08bad6886a8b32b1db2abaaffafedd0e4833d9
Angjelo <angjelo.thoma@squer.io>
2025-02-20 11:41:18 +0100

    Adjust structure

--- src/main/java/junitdemo/junitdemo/App.java
+++ src/main/java/junitdemo/junitdemo/App.java
@@ -1,35 +1,26 @@
 package junitdemo.junitdemo;
 
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println(getHelloWorld());
+
+    }
+
+    public static String getHelloWorld() {
+
+        return "Hello World";
+
+    }
+
+    public static String getHelloWorld2() {
+
+        return "Hello World 2";
+
+    }
 
 
-
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-
-	public static void main(String[] args) {
-
-		System.out.println(getHelloWorld());
-
-	}
-
-	public static String getHelloWorld() {
-
-		return "Hello World";
-
-	}
-
-	public static String getHelloWorld2() {
-
-		return "Hello World 2";
-
-	}
-
-	
 }
 
 
--- src/test/java/junitdemo/junitdemo/AppTest.java
+++ src/test/java/junitdemo/junitdemo/AppTest.java
@@ -1,22 +1,15 @@
 package junitdemo.junitdemo;
 
-
-
 import org.junit.Assert;
 import org.junit.Test;
 
+public class AppTest {
+    @Test
+    public void testPrintHelloWorld() {
 
+        Assert.assertEquals(App.getHelloWorld2(), "Hello World 2");
 
+    }
 
-
-
-public class AppTest{
-	@Test
-	public void testPrintHelloWorld() {
-
-	Assert.assertEquals(App.getHelloWorld2(), "Hello World 2");
-
-	}
-	
 } 
 
